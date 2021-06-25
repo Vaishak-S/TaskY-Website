d65643e9236a6d0dc6d88addd719fd1e9a196325
Vaishak-S <vaishak.satheesh@gmail.com>
2021-06-25 19:28:20 +0530

    Almost completed

---
+++ index.js
@@ -0,0 +1,42 @@
+const taskContainer = document.querySelector(".task__container");
+
+
+const generateNewCard = (taskData) => `
+<div class="col-md-6 col-lg-4" id="${taskData.id}">
+    <div class="card">
+      <div class="card-header d-flex justify-content-end gap-2">
+        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>   
+      </div>
+
+      <img src="${taskData.imageUrl}"
+       class="card-img-top p-3" 
+       alt="card image"
+       >
+
+      <div class="card-body">
+        <h5 class="card-title">${taskData.taskTitle}</h5>
+        <p class="card-text">${taskData.taskDescription}</p>
+        <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+      </div>
+
+      <div class="card-footer ">
+        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+      </div>
+      
+    </div>
+  </div>
+  `;
+
+
+const saveChanges = () =>{
+    const taskData = {
+        id: `${Date.now()}`, //generates unique number for id every second
+        imageUrl: document.getElementById("imageurl").value,
+        tastTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+    };
+
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+};
