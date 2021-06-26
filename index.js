6e14e912a788cb15ef63ffe4a333e2af890d9052
Vaishak-S <vaishak.satheesh@gmail.com>
2021-06-26 19:21:02 +0530

    delete feature implemented

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+var globalStore = [];
 
 const generateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4" id="${taskData.id}">
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-2">
         <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>   
+        <button type="button" class="btn btn-outline-danger" id="${taskData.id}" onclick="deleteCard.apply(this, arguments)">
+        <i class="fas fa-trash-alt" id="${taskData.id}" onclick="deleteCard.apply(this, arguments)"></i>
+        </button>   
       </div>
 
       <img src="${taskData.imageUrl}"
@@ -22,7 +24,7 @@ const generateNewCard = (taskData) => `
       </div>
 
       <div class="card-footer ">
-        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        <button type="button" class="btn btn-out line-primary float-end">Open Task</button>
       </div>
       
     </div>
@@ -34,11 +36,13 @@ const generateNewCard = (taskData) => `
     //localstorage to get tasky card data
     const getCardData = localStorage.getItem("tasky");
 
-    //convert to normal object
+    //convert stringified object to normal object
     const {cards} = JSON.parse(getCardData);
 
-    //loop over those array of task objects to create html card and inject it to the DOM
+    //loop over those array of task objects to create html card 
     cards.map((cardObject) => {
+
+      //inject it to the DOM
       taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
       
       //updating our globalStore
@@ -64,7 +68,25 @@ const saveChanges = () =>{
       , JSON.stringify/* JSON - to javasript objects // stringify - converts to string */
       ({cars:globalStore}) );     //setItem() - add item to local storage
 
-
-
-
   };
+
+  //delete feature of each card
+  const deleteCard = (event) => {
+    event=window.event;
+   
+    //to get Id of the clicked card
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+    
+    //match the Id with the element in the globalstore
+    //If match found, then remove that card
+    const newUpdatedArray = globalStore.filter((cardObject) => cardObject.id != targetID);
+    globalstore = newUpdatedArray;//copied updated array to the original array globalStore
+    localStorage.setItem("tasky" , JSON.stringify({cars:globalStore}));
+    
+    //contacting parent
+    if( tagname == "BUTTON")
+    return     taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    else
+    return     taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }; 
